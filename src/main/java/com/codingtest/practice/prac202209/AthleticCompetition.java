be4db46933e2403d06cc3c9962516fdd307fe69e
Soo77 <shimsh3@naver.com>
2022-09-26 23:43:29 +0900

    학생 구분을 위해 그냥 점수배열에서 학생:점수 형태의 MAP으로 변경중

--- src/main/java/com/codingtest/practice/prac202209/AthleticCompetition.java
+++ src/main/java/com/codingtest/practice/prac202209/AthleticCompetition.java
@@ -1,6 +1,9 @@
 package com.codingtest.practice.prac202209;
 
-import java.util.*;
+import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class AthleticCompetition {
 
@@ -17,22 +20,25 @@ public class AthleticCompetition {
             cutLen = studentCnt;
         }
 
-        int[][] score = new int[sportsCnt][studentCnt];
-        //int[][] score = new int[studentCnt][sportsCnt];
+        // 20220926 hashmap List가 있어야할것 같다.
+        List<HashMap<Integer,Integer>> studentScoreList = new ArrayList<>();
 
+
+        //학생구분을 위한 "학생번호:점수" 식으로 map으로 변경
         for(int i=0; i<sportsCnt; i++) {
+            HashMap<Integer,Integer> studentScoreMap = new HashMap<>();
             for(int j=0; j<studentCnt; j++) {
-                score[i][j] = ability[j][i];
+                studentScoreMap.put(j+1,ability[j][i]);
             }
+            studentScoreList.add(studentScoreMap);
         }
 
         //java.util.Arrays.deepToString() 메소드를 이용하면
         //간단하게 2차원 배열의 값을 문자열로 반환받아서 출력할 수 있다.
-        System.out.println("이차원배열뽑기:"+Arrays.deepToString(score));
-        System.out.println("그럼 이건 몬데?"+Arrays.toString(score[0]));
-        System.out.println("그럼 이건 몬데?"+Arrays.toString(score[1]));
 
-        //내림차순 정렬
+        System.out.println("List뽑아보기:"+studentScoreList.toString());
+
+        //int 배열 : 내림차순 정렬
         //방법1
         //Integer[] IntegerArr = Arrays.stream(arr).boxed().toArray(Integer[]::new);
         //Arrays.sort(IntegerArr, Collections.reverseOrder());
@@ -41,12 +47,16 @@ public class AthleticCompetition {
         //arr = Arrays.stream(IntegerArr).boxed().sorted(Collections.reverseOrder())
         //.mapToInt(Integer::intValue).toArray();
 
-        for(int i=0; i<sportsCnt; i++) {
-            Integer[] tempArr = Arrays.stream(score[i]).boxed().toArray(Integer[]::new);
-            Arrays.sort(tempArr, Collections.reverseOrder());
-            score[i] = Arrays.stream(tempArr).mapToInt(Integer::intValue).toArray();
+        //string 배열 : 내림차순 정렬
+        // Arrays.sort(arr2, Collections.reverseOrder());
+
+        //MAP value로 내림차순 정렬
+        List<HashMap<Integer,Integer>> sortedMapList = new ArrayList<>();
+        for(int i=0; i<studentScoreList.size(); i++) {
+            List<Entry<Integer,Integer>> tempEntryList = new ArrayList<Entry<Integer,Integer>>(studentScoreList.get(i).entrySet());
+
+
         }
-        System.out.println("뽑아보갔어:"+Arrays.deepToString(score));
 
         //종목수만큼 짜르기(꼭 안해도될 비교하지않게)
         //배열자르기
@@ -56,12 +66,15 @@ public class AthleticCompetition {
         //int[] arr2 = Arrays.copyOfRange(arr,position,arr.length);
         //arr1 -> [0,1,2]
         //arr2 -> [3,4,5]
+/*
         for(int i=0; i<sportsCnt; i++) {
             score[i] = Arrays.copyOfRange(score[i],0,cutLen);
         }
         System.out.println("자른후:"+Arrays.deepToString(score));
+*/
         //오 잘 잘라졌다!!! 뜨헉
         //아 사람 안겹치게 해야하는데...ㅎ. .ㅎ.ㅎ.어떻게해야하지?
+
 
 
 
