3c8d00a1d4b3771a28ef504477bc5ce44341c64b
Soo77 <shimsh3@naver.com>
2022-10-13 22:25:05 +0900

    그리디 알고리즘 예시: 거스름 돈

---
+++ src/main/java/com/codingtest/practice/etc/Greedy.java
@@ -0,0 +1,25 @@
+package com.codingtest.practice.etc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//그리디 알고리즘 예시
+public class Greedy {
+
+    public static void main(String[] args) {
+
+        int n = 1260;
+        int cnt=0;
+
+        int coinTypes[] = {500, 100, 50, 10};
+
+        for(int i=0; i<coinTypes.length; i++) {
+            cnt += n / coinTypes[i];
+            n %= coinTypes[i];
+        }
+        System.out.println(cnt);
+
+
+
+    }
+}
