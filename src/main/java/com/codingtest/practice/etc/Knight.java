629d806150d748bbfe14cec49d7a283f5c5bbb85
Soo77 <shimsh3@naver.com>
2022-10-25 23:06:36 +0900

    그리디 알고리즘: 왕실의 나이트

---
+++ src/main/java/com/codingtest/practice/etc/Knight.java
@@ -0,0 +1,16 @@
+package com.codingtest.practice.etc;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+//이코테 따라하기
+//그리디 알고리즘 예시
+// 왕실의 나이트
+public class Knight {
+
+    public static void main(String[] args) {
+
+
+    }
+}
