0e38eff178240597beccbb01dd4b9a9a937a0214
Soo77 <shimsh3@naver.com>
2022-11-03 23:04:45 +0900

    이해가 안가는 2504 괄호의값....

---
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/part1/n2504.java
@@ -0,0 +1,58 @@
+package com.codingtest.practice.baekjoon.Algorithm.part1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class n2504 {
+
+    // 괄호의 값
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Stack<Character> stack = new Stack<>();
+
+        String input = br.readLine();
+        char arr[] = input.toCharArray();
+        int ans = 0;
+        int tmp = 1;
+        for(int i=0; i<arr.length; i++) {
+            char c = arr[i];
+            if(c=='(' || c=='[') {
+                stack.add(c);
+                tmp *= (c=='(') ? 2 : 3;
+            } else {
+                char cc = stack.pop();
+                if(c==')') {
+                    if(cc != '(') {
+                        ans = 0;
+                        break;
+                    } else {
+                        if(arr[i-1] == '(') {
+                            ans += tmp;
+                        }
+                        tmp /= 2;
+                    }
+                }  else {
+                    if(cc != '[') {
+                        ans = 0;
+                        break;
+                    } else {
+                        if(arr[i-1] == '[') {
+                            ans += tmp;
+                        }
+                        tmp /= 3;
+                    }
+                }
+            }
+        }
+        if(!stack.isEmpty())
+            ans = 0;
+        System.out.println(ans);
+
+    }
+}
