3cb34f73daca08e52e4141b077ccbdba48f1b5cd
Soo77 <shimsh3@naver.com>
2022-11-07 21:40:05 +0900

    다음문제는 가르침 (남극단어)

---
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/part1/n1062.java
@@ -0,0 +1,20 @@
+package com.codingtest.practice.baekjoon.Algorithm.part1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class n1062 {
+
+    // 가르침(남극학생)
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        System.out.println('n'-'a');
+        System.out.println('z'-'a');
+    }
+}
