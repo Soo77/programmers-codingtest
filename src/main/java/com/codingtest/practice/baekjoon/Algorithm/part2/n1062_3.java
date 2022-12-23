76ec4eeda50f328a5c6f417f1709bf17aac8cd44
Soo77 <shimsh3@naver.com>
2022-12-24 02:40:40 +0900

    헤매고헤맨 가르침

---
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/part2/n1062_3.java
@@ -0,0 +1,120 @@
+package com.codingtest.practice.baekjoon.Algorithm.part2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class n1062_3 {
+
+    // 가르침(남극학생)
+
+    static int N,K;
+    static int max= Integer.MIN_VALUE;
+    static boolean visited[] = new boolean[26];
+    static String[] strArr;
+
+    public static void dfs(int start, int count) {
+
+        //int cnt = 0;
+        if (count == K - 5) {
+
+            int cnt = 0;
+            for (int i = 0; i < strArr.length; i++) {
+                boolean chk = true;
+                for (int j = 0; j < strArr[i].length(); j++) {
+                    //char ch = strArr[i].charAt(j);
+                    //System.out.println("visited[ch - 'a']:"+visited[ch - 'a']);
+                    if (!visited[strArr[i].charAt(j) - 'a']) {
+                        chk = false;
+                        System.out.println("이건못읽어유:" + strArr[i]);
+                        break;
+                    }
+                }
+                if (chk) cnt++;
+            }
+
+            System.out.println("max:" + max);
+            max = Math.max(max, cnt);
+            return;
+        }
+
+
+        for (int i = start; i < 26; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(i, count+1);
+                visited[i] = false;
+            }
+
+        }
+        /*static void check(int alpha, int count) {
+            if (count == K - 5) {
+                int temp = 0;
+                for (int i = 0; i < N; i++) {
+                    boolean flag = true;
+
+                    for (int j = 0; j < strArr[i].length(); j++) {
+                        *//* 배우지않은 알파벳이 있는 경우 *//*
+                        if (!visited[strArr[i].charAt(j) - 'a']) {
+                            flag = false;
+                            break;
+                        }
+                    }
+
+                    if (flag) {
+                        temp++;
+                    }
+                }
+                max = Math.max(temp, max);
+                return;
+            }
+
+            for (int i = alpha; i < 26; i++) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    check(i, count + 1);
+                    visited[i] = false;
+                }
+            }
+        }*/
+    }
+
+    public static void main(String[] args) throws Exception {
+
+/*        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());*/
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        strArr = new String[N];
+
+        if(K < 5) {
+            max = 0;
+        } else if(K == 26) {
+            max = N;
+        } else {
+            for(int i=0; i<N; i++) {
+                strArr[i]=br.readLine().replaceAll("anta|tica","");
+            }
+
+            visited['a' - 'a'] = true;
+            visited['n' - 'a'] = true;
+            visited['t' - 'a'] = true;
+            visited['i' - 'a'] = true;
+            visited['c' - 'a'] = true;
+
+        }
+
+        dfs(0,0);
+        System.out.println("답:"+max);
+    }
+
+}
