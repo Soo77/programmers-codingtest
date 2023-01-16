65e5b7f734e1db0a21a27fb466524046db72b575
Soo77 <shimsh3@naver.com>
2023-01-17 00:09:17 +0900

    자바의 정석 연습문제 9-5

--- src/main/java/com/codingtest/practice/Java3rdEdition/exercise/Excercise9_4.java
+++ src/main/java/com/codingtest/practice/Java3rdEdition/exercise/Excercise9_4.java
@@ -1,7 +1,16 @@
 package com.codingtest.practice.Java3rdEdition.exercise;
 
 public class Excercise9_4 {
-    public static void main(String[] args) {
 
+    static void printGraph(int[] dataArr, char ch) {
+        for(int i=0; i<dataArr.length; i++) {
+            for(int j=0; j<dataArr[i]; j++) {
+                System.out.print(ch);
+            }
+            System.out.println(dataArr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        printGraph(new int[]{3,7,1,4}, '*');
     }
 }
