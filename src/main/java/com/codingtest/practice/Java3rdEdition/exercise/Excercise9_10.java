493e4c3eab6bb91432f98870cda089376916b5bc
Soo77 <shimsh3@naver.com>
2023-01-20 01:17:56 +0900

    커맨드라인으로 2~9사이의 두개의 숫자 받아서 구구단 출력 ~_~

--- src/main/java/com/codingtest/practice/Java3rdEdition/exercise/Excercise9_10.java
+++ src/main/java/com/codingtest/practice/Java3rdEdition/exercise/Excercise9_10.java
@@ -1,43 +1,35 @@
 package com.codingtest.practice.Java3rdEdition.exercise;
 
-import java.util.Arrays;
-
 /*
 * 주어진 문자열을 지정된 크기의 문자열로 변환한다 나머지 공간은 공백으로 채운다
  * */
 public class Excercise9_10 {
 
     private static String format(String str, int length, int alignment) {
+        int diff = length - str.length();
+        if(diff < 0) return str.substring(0, length);
 
-        if(length < str.length()) {
-            return str.substring(0,length);
-        } else {
-            char[] strArr = str.toCharArray();
-            char[] c = new char[length];
-            for(int i=0; i<c.length; i++) {
-                c[i] = (char)(i + '0');
-            }
-            //System.arraycopy(strArr, 0, c, alignment, str.length());
-            System.out.println(Arrays.toString(strArr));
-            System.out.println(Arrays.toString(c));
-            switch(alignment) {
-                case 0 :
-                    System.arraycopy(strArr, 0, c, 0, str.length());
-                    break;
-                case 1 :
-                    System.arraycopy(strArr, 0, c, (length-str.length())/2, str.length());
-                    break;
-                case 2 :
-                    System.arraycopy(strArr, 0, c, length-str.length(), str.length());
-                    break;
-                default:
-                    break;
-            }
-            return new String(c);
+        char[] strArr = str.toCharArray();
+        char[] dest = new char[length];
+        for(int i=0; i<dest.length; i++) {
+            dest[i] = (char)(i + '0');
         }
 
+        switch (alignment) {
+            case 0 :
+                System.arraycopy(strArr, 0, dest, 0, str.length());
+                break;
+            case 1 :
+                System.arraycopy(strArr, 0, dest, diff/2, str.length());
+                break;
+            case 2 :
+                System.arraycopy(strArr, 0, dest, diff, str.length());
+                break;
+        }
 
+        return new String(dest);
     }
+
 
     public static void main(String[] args) {
         String str = "가나다";
@@ -45,7 +37,6 @@ public class Excercise9_10 {
         System.out.println(format(str,7,0));
         System.out.println(format(str,7,1));
         System.out.println(format(str,7,2));
-        System.out.println(format("가나다라",14,1));
     }
 
 }
