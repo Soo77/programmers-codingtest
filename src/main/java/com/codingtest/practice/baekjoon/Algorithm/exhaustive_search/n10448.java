6586abbf3350cfe35cb7f2b555f0a7831d89bbcd
Soo77 <shimsh3@naver.com>
2023-06-21 21:55:07 +0900

    완전탐색 알고리즘 10448번 유레카이론

---
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/exhaustive_search/n10448.java
@@ -0,0 +1,45 @@
+package com.codingtest.practice.baekjoon.Algorithm.exhaustive_search;
+
+import java.io.*;
+
+public class n10448 {
+
+    private static int findTriNum(int num) {
+        int max = (num/2)+1;
+        //System.out.println("max:"+max);
+
+        for(int i=1; i<max; i++) {
+            for(int j=i; j<max; j++) {
+                for(int k=j; k<max; k++) {
+                    if((i*(i+1) + j*(j+1) + k*(k+1)) == (num*2)) {
+                        return 1;
+                    }
+                }
+            }
+        }
+        //System.out.println();
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++) {
+            int result = findTriNum(Integer.parseInt(br.readLine()));
+            bw.write(result+"\n");
+        }
+
+
+
+
+        bw.flush();
+
+
+    }
+
+
+}
