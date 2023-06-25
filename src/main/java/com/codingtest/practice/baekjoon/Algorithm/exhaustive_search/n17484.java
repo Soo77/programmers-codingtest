378c67c81096b6372cb8187d81610d44e492f53d
Soo77 <shimsh3@naver.com>
2023-06-25 23:39:48 +0900

    DP(다이나믹 프로그래밍)

--- src/main/java/com/codingtest/practice/baekjoon/Algorithm/exhaustive_search/n17484.java
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/exhaustive_search/n17484.java
@@ -2,6 +2,7 @@ package com.codingtest.practice.baekjoon.Algorithm.exhaustive_search;
 import java.io.*;
 import java.util.StringTokenizer;
 
+//DP
 public class n17484 {
     public static void main(String[] args) throws IOException {
 
@@ -12,23 +13,21 @@ public class n17484 {
         int row = Integer.parseInt(st.nextToken());
         int col = Integer.parseInt(st.nextToken());
 
+        int fuel[][] = new int[row][col];
         for(int i=0; i<row; i++) {
-        	st = new StringTokenizer(br.readLine());
-
+			st = new StringTokenizer(br.readLine());
+        	for(int j=0; j<col; j++) {
+        		fuel[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
-		String[] chess = new String[row];
-        for(int i=0; i<row; i++) {
-            chess[i] = br.readLine();
-        }
+
 
 		int minVal = Integer.MAX_VALUE;
-		for(int i=0; i<=row-8; i++) {
-			for(int j=0; j<=col-8; j++) {
-				int result = findMinVal(i, j, chess);
+		for(int i=0; i<=col; i++) {
+			int result = 0;
 
-				if(minVal > result) {
-					minVal = result;
-				}
+			if(minVal > result) {
+				minVal = result;
 			}
 		}
 
@@ -36,19 +35,6 @@ public class n17484 {
 		bw.flush();
 	}
 
-	private static int findMinVal(int startRow, int startCol, String[] chess) {
-		String[] orgChess = {"WBWBWBWB", "BWBWBWBW"};
-		int Wcount = 0;
-		for(int row=startRow; row<startRow + 8; row++) {
-			for(int col=startCol; col<startCol + 8; col++) {
-				if(chess[row].charAt(col) != orgChess[row%2].charAt(col%8)) {
-					Wcount++;
-				}
-			}
-		}
-
-		return Math.min(Wcount, 64 - Wcount);
-	}
 
        
 }
