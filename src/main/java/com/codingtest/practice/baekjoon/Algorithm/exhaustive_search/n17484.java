3ea4036d4e29ee500cddd3667543b272e2b18363
Soo77 <shimsh3@naver.com>
2023-06-26 21:25:27 +0900

    dfs 진우의 달

--- src/main/java/com/codingtest/practice/baekjoon/Algorithm/exhaustive_search/n17484.java
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/exhaustive_search/n17484.java
@@ -3,17 +3,37 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 //DP
+/*
+* DP(다이나믹 프로그래밍)
+* 하나의 큰 문제를 여러 개의 작은 문제로 나누어서 그 결과를
+* 저장하여 다시 큰 문제를 해결할 때 사용
+*
+* DP가 적용되기 위해서는 2가지 조건을 만족해야함.
+* 1) Overlapping Subproblems(겹치는 부분 문제)
+* 2) Optimal Substructure(최적 부분 구조)
+*
+*
+*
+* */
 public class n17484 {
+
+	static int min = Integer.MAX_VALUE;
+	static int fuel[][];
+	static int[] ydir = {-1, 0, 1};
+	static int[] visited;
+
+	static int row, col;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int row = Integer.parseInt(st.nextToken());
-        int col = Integer.parseInt(st.nextToken());
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
 
-        int fuel[][] = new int[row][col];
+        fuel = new int[row][col];
         for(int i=0; i<row; i++) {
 			st = new StringTokenizer(br.readLine());
         	for(int j=0; j<col; j++) {
@@ -21,21 +41,45 @@ public class n17484 {
 			}
 		}
 
-
-		int minVal = Integer.MAX_VALUE;
-		for(int i=0; i<=col; i++) {
-			int result = 0;
-
-			if(minVal > result) {
-				minVal = result;
-			}
+        for(int i=0; i<col; i++) {
+			visited = new int[row];
+			visited[0] = i;
+			dfs(1, i, -1);
 		}
 
-		bw.write(minVal + "");
+
+
+
+		bw.write( min+"");
 		bw.flush();
 	}
 
+	private static void dfs(int depth, int y, int dir) {
+		if(depth == row) {
+			int sum = fuel[0][visited[0]];
+			for(int i=1; i<row; i++) {
+				sum += fuel[i][visited[i]];
+			}
 
-       
+			min = min > sum ? sum : min;
+			return;
+		}
+
+		for(int i=0; i<3; i++) {
+			int dy = y + ydir[i];
+			if(isValidPosition(dy) && dir != i) {
+				visited[depth] = dy;
+				dfs(depth+1, dy, i);
+			}
+		}
+	}
+
+	private static boolean isValidPosition(int y) {
+    	if(y<0 || y>=col)
+    		return false;
+    	return true;
+	}
+
+
 }
 
