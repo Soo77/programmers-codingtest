d14d270356474bb8774c10b3cb60557c73c23372
Soo77 <shimsh3@naver.com>
2023-07-01 23:26:32 +0900

    1182 부분수열의 합

---
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/exhaustive_search/n1182.java
@@ -0,0 +1,41 @@
+package com.codingtest.practice.baekjoon.Algorithm.exhaustive_search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class n1182 {
+
+
+	static int N, target;
+	static int count;
+	static int[] arr;
+	public static void dfs(int index, int sum){
+		if(index == N){
+			if(sum == target){
+				count++;
+			}
+			return;
+		}
+		System.out.print(arr[index]+",");
+		dfs(index + 1, sum + arr[index]);
+		dfs(index + 1, sum);
+		System.out.println();
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		target = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		dfs(0, 0);
+
+		System.out.println(target == 0 ? count - 1 : count);
+	}
+}
