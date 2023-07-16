cf600279d650eae3b92980d05eb626e39ad80d86
Soo77 <shimsh3@naver.com>
2023-07-16 18:14:31 +0900

    2583 영역구하기 dfs

--- src/main/java/com/codingtest/practice/baekjoon/Algorithm/dfs/n2583_submit.java
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/dfs/n2583_submit.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 import java.util.StringTokenizer;
 
 
-public class n2583 {
+public class n2583_submit {
 
     //영역 구하기
 
@@ -34,95 +34,29 @@ public class n2583 {
         map = new int[N+1][M+1];
         visited = new boolean[N+1][M+1];
 
-
-
-        /*for(int i=1; i<=N; i++) {
-            String s = br.readLine();
-            for(int j=1; j<=N; j++) {
-                map[i][j] = s.charAt(j-1) - '0';
-            }
-        }*/
-
-
-
-
-
-        System.out.println();
-        for(int i=0; i<=N; i++) {
-            for(int j=0; j<=M; j++) {
-                System.out.print("("+i+","+j+"), ");
-            }
-            System.out.println();
-        }
-        System.out.println("end");
-
-
         int x=0,y=0;
         int x1=0,y1=0;
         for(int i=0; i<K; i++) {
             st = new StringTokenizer(br.readLine());
             x = Integer.parseInt(st.nextToken());
             y = Integer.parseInt(st.nextToken());
-            System.out.print("("+x+","+y + ") ,");
-            //map[x+1][y] = 1;
 
             x1 = Integer.parseInt(st.nextToken());
             y1 = Integer.parseInt(st.nextToken());
-            System.out.print("("+x1+","+y1 + ") ,");
-            //map[x1][y1-1] = 1;
 
-            System.out.println("\nx:"+x+",x1:"+x1);
-            System.out.println("y:"+y+",y1:"+y1);
             for(int j=y; j<y1; j++) {
-                System.out.println("j:"+j+"@@@@@@@@@@@@@@@@@@");
                 for(int k=x; k<x1; k++) {
-                    System.out.println("k:"+k+",j:"+j);
-                    System.out.println("map "+k+","+j+":에게 부여한다");
                     map[k][j]= 1;
                 }
             }
-
-
-                System.out.println();
         }
-
-        System.out.println();
-        for(int i=0; i<=N; i++) {
-            for(int j=0; j<=M; j++) {
-                System.out.print("("+i+","+j+"), ");
-            }
-            System.out.println();
-        }
-        System.out.println("end");
-
-        System.out.println();
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                System.out.print(map[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println("end");
-
-        System.out.println();
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                System.out.print("("+i+","+j+"), ");
-            }
-            System.out.println();
-        }
-        System.out.println("end");
-
-
 
         for(int i=0; i<N; i++) {
             for(int j=0; j<M; j++) {
                 if (map[i][j] == 0 && !visited[i][j]) {
                     area = 1;
                     dfs(i, j);
-                    System.out.println("area:"+area);
                     count++;
-
                     areaList.add(area);
                 }
 
