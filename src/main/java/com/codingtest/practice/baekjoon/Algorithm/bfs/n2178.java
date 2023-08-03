f2ebb96cb820676d0cbc5a31b4a6479cbf5d11ef
Soo77 <shimsh3@naver.com>
2023-08-04 02:09:45 +0900

    int array객체의 큐를 출력하기위해 toString override를 하여따

--- src/main/java/com/codingtest/practice/baekjoon/Algorithm/bfs/n2178.java
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/bfs/n2178.java
@@ -2,9 +2,11 @@ package com.codingtest.practice.baekjoon.Algorithm.bfs;
 
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 // 미로탐색 //아직 작성중
 public class n2178 {
@@ -13,8 +15,19 @@ public class n2178 {
     static int map[][];
     static boolean visited[][];
     static int count = 0;
+    static int sum = 0;
 
-    static Queue<int[]> que = new LinkedList<>();
+    //static Queue<int[]> que = new LinkedList<>();
+    //que 출력해보려고..
+    static Queue<int[]> que = new LinkedList<>() {
+        @Override public String toString() {
+            return "[" +
+                    stream()
+                            .map(Arrays::toString)
+                            .collect(Collectors.joining(", ")) +
+                    "]";
+        }
+    };
 
     public static void main(String[] args) throws IOException {
 
@@ -42,13 +55,22 @@ public class n2178 {
             System.out.println();
         }
 
+        for(int i=1; i<=N; i++) {
+            for(int j=1; j<=M; j++) {
+                System.out.print("("+i+","+j+")" + ",");
+            }
+            System.out.println();
+        }
+
         bfs(1,1);
 
         System.out.println("count:"+count);
+        System.out.println("sum:"+sum);
 
     }
 
     private static void bfs(int startX, int startY) {
+        System.out.println("현재:" + startX+","+startY);
         visited[startX][startY] = true;
         que.add(new int[] {startX, startY});
 
@@ -59,18 +81,23 @@ public class n2178 {
 
         while(!que.isEmpty()) {
             int[] poll = que.poll();
+            System.out.println("poll:"+Arrays.toString(poll));
+
 
             for(int i=0; i<4; i++) {
                 int x = poll[0] + X[i];
                 int y = poll[1] + Y[i];
 
-                if(x<0 || x>= N || y<0 || y>=M) {
+                if(x<=0 || x> N || y<=0 || y>M) {
                     continue;
                 }
 
                 if(map[x][y] == 1 && !visited[x][y]) {
                     que.add(new int[] {x, y});
-                    count++;
+
+                    System.out.println("que:"+que);
+                    System.out.println("size:"+que.size());
+                    sum += que.size();
 
                     visited[x][y] = true;
                 }
