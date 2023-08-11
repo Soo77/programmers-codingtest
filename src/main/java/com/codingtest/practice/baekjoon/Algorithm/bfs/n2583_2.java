242ffb31c2569031a96ff245ad5a3e3a8f6fd81a
Soo77 <shimsh3@naver.com>
2023-08-11 23:43:44 +0900

    2583 다시풀기

---
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/bfs/n2583_2.java
@@ -0,0 +1,24 @@
+package com.codingtest.practice.baekjoon.Algorithm.bfs;
+
+
+import java.io.*;
+import java.util.*;
+
+// 영역 구하기
+public class n2583_2 {
+
+
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+
+
+}
