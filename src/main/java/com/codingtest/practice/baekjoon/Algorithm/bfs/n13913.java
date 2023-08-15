34464581e68011c1eb6989adb35c05b61998442d
Soo77 <shimsh3@naver.com>
2023-08-16 07:43:47 +0900

    숨바꼭질4. . . . .ㅜ-ㅜ

--- src/main/java/com/codingtest/practice/baekjoon/Algorithm/bfs/n13913.java
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/bfs/n13913.java
@@ -10,6 +10,8 @@ import java.util.StringTokenizer;
 public class n13913 {
 
     static int N,K;
+    static int[] parent = new int[100001];
+    static int[] time = new int[100001];
 
     public static void main(String[] args) throws IOException {
 
@@ -20,19 +22,38 @@ public class n13913 {
         N = Integer.parseInt(st.nextToken()); // 수빈
         K = Integer.parseInt(st.nextToken()); // 동생
 
-        bfs(N);
+        bfs();
 
     }
 
-    private static void bfs(int start) {
+    private static void bfs() {
 
         Queue<Integer> que = new LinkedList<Integer>();
-        int[] visited = new int[100001];
-        que.add(start);
+        que.add(N);
+        time[N] = 1;
 
         while(!que.isEmpty()) {
-            int pos = que.poll();
-            System.out.println("pos:"+pos);
+            int now = que.poll();
+
+            if(now == K) return;
+
+            for(int i=0; i<3; i++) {
+                int next;
+
+                if(i==0) next = now + 1;
+                else if(i==1) next = now - 1;
+                else next = now*2;
+
+                if(next < 0 || next > 100000) continue;
+
+                if(time[next] == 0) {
+                    que.add(next);
+                    time[next] = time[now] + 1;
+                    parent[next] = now;
+                }
+            }
+
+
 
         }
 
