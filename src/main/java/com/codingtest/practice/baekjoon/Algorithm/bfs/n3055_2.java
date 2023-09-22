14b8226092acf06223c324936ac5f030572b05c9
Soo77 <shimsh3@naver.com>
2023-09-23 02:08:46 +0900

    3055 고심이 탈출 재시도

--- src/main/java/com/codingtest/practice/baekjoon/Algorithm/bfs/n3055_2.java
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/bfs/n3055_2.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class n3055 {
+public class n3055_2 {
 
 	static int R,C;
 	static char map[][];
@@ -46,52 +46,48 @@ public class n3055 {
 	private static void bfs() {
 		while(!sq.isEmpty()) {
 
-			// 물 이동동
-			for(int i = 0, w_len = wq.size(); i<w_len; i++) {
-				int cur_w[] = wq.poll();
-				int x = cur_w[0];
-				int y = cur_w[1];
+			// 물 먼저 옮기기
+			for(int i=0, w_len = wq.size(); i<w_len; i++) {
+				int poll[] = wq.poll();
+				int x = poll[0];
+				int y = poll[1];
+
 				for(int j=0; j<4; j++) {
 					int nx = x + dx[j];
 					int ny = y + dy[j];
 
-					if(nx >=0 && nx < R && ny >= 0 && ny < C && map[nx][ny] == '.') {
-						// 물의 새로운 좌표를 물로 채움
-						map[nx][ny] = '*';
+					if(nx >= 0 && nx < R && ny >= 0 && ny < C && map[nx][ny] == '.') {
 
-						// 물의 새로운 좌표를 물큐에 넣어줌
+						map[nx][ny]  = '*';
 						wq.add(new int[]{nx,ny});
 					}
 				}
 			}
 
-			// 고슴도치 이동
+			// 고심이 이동
 			for(int i=0, s_len = sq.size(); i<s_len; i++) {
-				int[] cur_s = sq.poll();
-				int x = cur_s[0];
-				int y = cur_s[1];
+				int poll[] = sq.poll();
+				int x = poll[0];
+				int y = poll[1];
+
 				for(int j=0; j<4; j++) {
 					int nx = x + dx[j];
 					int ny = y + dy[j];
-					int time = cur_s[2];
+					int time = poll[2];
 
-					// 고슴이의 새로운 좌표가 map 범위안에 있따면
-					if(nx >= 0 && nx<R && ny >=0 && ny < C) {
-						// 고심이의 새 좌표가 비버굴에 도착했으면 종료
+					if(nx >=0 && nx < R && ny >= 0 && ny < C) {
 						if(map[nx][ny] == 'D') {
-							ans = Math.min(ans,time+1);
+							ans = Math.min(ans, time+1);
 							return;
-						}
-
-						// 고심이 새 좌표가 갈 수 있는 공간이면
-						else if(map[nx][ny] == '.') {
-							map[nx][ny] = 'S';
+						} else if(map[nx][ny] == '.') {
 							sq.add(new int[]{nx,ny,time+1});
 						}
 					}
 				}
 			}
 		}
+
+
 	}
 
 
