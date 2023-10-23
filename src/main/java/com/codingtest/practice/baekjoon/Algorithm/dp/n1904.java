ad796f2bf974b0eb5709851f20f86b52904fe50d
Soo77 <51576144+Soo77@users.noreply.github.com>
2023-10-23 10:40:43 +0900

    Create n1904.java 01타일 dp

---
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/dp/n1904.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class n1904 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+		int N = Integer.parseInt(br.readLine());
+ 		int dp[] = new int[N+1];
+        dp[0] = 1;
+		dp[1] = 2;
+ 		for(int i=2; i<=N; i++) {
+			dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+		}
+ 		bw.write(dp[N-1]+"");
+		bw.flush();
+    }
+}
