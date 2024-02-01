70b658c07a6d5e74d74f8b31e60b59af1af91ad8
Soo77 <51576144+Soo77@users.noreply.github.com>
2024-02-01 17:08:21 +0900

    Update n1522.java

--- src/main/java/com/codingtest/practice/baekjoon/Algorithm/random/n1522.java
+++ src/main/java/com/codingtest/practice/baekjoon/Algorithm/random/n1522.java
@@ -1,6 +1,7 @@
 package com.codingtest.practice.baekjoon.Algorithm.random;
 
 
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -19,4 +20,30 @@ public class n1522 {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     
-    String str = br.readLine()
+    String str = br.readLine();
+    
+    int min = Integer.MAX_VALUE;
+    
+    int aCnt = 0;
+    for(int i=0; i<str.length(); i++) {
+      if(str.charAt(i) == 'a') {
+        aCnt++;
+      }
+    }
+    
+    for(int i=0; i<str.length(); i++) {
+      int bCnt = 0;
+      for(int j=i; j<i+aCnt; j++) {
+        int idx = j%str.length();
+        if(str.charAt(idx)=='b') {
+          bCnt++;
+        }
+        
+      }
+      min = Math.min(min, bCnt);
+    }
+    System.out.println(min);
+  }
+
+ 
+}
